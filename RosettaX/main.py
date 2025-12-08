0444d994d251218c1d91adb2963a21152d0c20f6
Victor Piaskowski <victor.piaskowski@proton.me>
2025-12-08 17:17:54 +0100

    main.y changes, change to individual pages format and layouting

--- RosettaX/main.py
+++ RosettaX/main.py
@@ -3,39 +3,13 @@ import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html, State, MATCH
 import os
 from dash import ALL
-from HTMLFrontend.styling import CONTENT_STYLE, SIDEBAR_STYLE
-from HTMLFrontend.sidebar import sidebar as sbar
-from HTMLFrontend.flourescent_calibration_layout import fluorescent_calibration_layout
-from HTMLFrontend.other_calibration_layout import other_calibration_layout
+from pages.styling import CONTENT_STYLE, SIDEBAR_STYLE
+from pages.sidebar import sidebar as sbar
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-# the style arguments for the sidebar. We use position:fixed and a fixed width
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-
-sidebar = html.Div(
-    sbar(),
-    style=SIDEBAR_STYLE(),
-)
-
-content = html.Div(id="page-content", style=CONTENT_STYLE())
-
-app.layout = html.Div([dcc.Location(id="url"), dcc.Store(id="start-calibration-store"), sidebar, content])
-
-
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-def render_page_content(pathname):
-    if pathname == "/":
-        return html.P("This is the content of the home page!")
-    elif pathname == "/page-1":
-        return fluorescent_calibration_layout()
-    elif pathname == "/page-2":
-        return other_calibration_layout()
-    # If the user tries to reach a different page, return a 404 message
-    else:
-        return html.P("This is the content of the calibration start page.")
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
+sidebar = html.Div(sbar(), id="sidebar", style=SIDEBAR_STYLE())
+content = html.Div(dash.page_container, id="page-content", style=CONTENT_STYLE())
+app.layout = html.Div([dcc.Location(id="url"), dcc.Store(id="apply-calibration-store"), sidebar, content])
 
 @app.callback(
     Output("collapse-card", "is_open"),
@@ -45,27 +19,26 @@ def render_page_content(pathname):
 )
 def toggle_collapse(n, is_open):
     if n:
-        print(f"Toggle collapse to {not is_open}")
         return not is_open
     return is_open
 
-@app.callback(
-    Output("start-calibration-store", "data"),
-    Input({"type": "start-calibration", "index": ALL}, "n_clicks"),
-    prevent_initial_call=True,
-)
-def start_calibration(n_clicks_list):
-    # ctx = dash.callback_context
-    # if not ctx.triggered:
-    #     raise dash.exceptions.PreventUpdate
-    # # triggered e.g. '{"type":"start-calibration","index":"folder/file"}.n_clicks'
-    # triggered_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    # id_dict = json.loads(triggered_id)
-    # index = id_dict["index"]
-    print(n_clicks_list)
-    index = None
-    print("Calibration started for:", index)
-    return {"index": index, "n_clicks": n_clicks_list}
+# @app.callback(
+#     Output("apply-calibration-store", "data"),
+#     Input({"type": "apply-calibration", "index": ALL}, "n_clicks"),
+#     prevent_initial_call=True,
+# )
+# def apply_calibration(n_clicks_list):
+#     ctx = dash.callback_context
+#     if not ctx.triggered:
+#         raise dash.exceptions.PreventUpdate
+#     # triggered e.g. '{"type":"apply-calibration","index":"folder/file"}.n_clicks'
+#     triggered_id = ctx.triggered[0]["prop_id"].split(".")[0]
+#     id_dict = json.loads(triggered_id)
+#     index = id_dict["index"]
+#     print(n_clicks_list)
+#     index = None
+#     print("Calibration started for:", index)
+#     return {"index": index, "n_clicks": n_clicks_list}
 
 if __name__ == "__main__":
-    app.run(port=8888)
+    app.run(debug=True)
