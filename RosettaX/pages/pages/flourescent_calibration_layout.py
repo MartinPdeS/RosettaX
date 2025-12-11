756f09b428f0ff081bd974f39a84007664dae29b
Victor Piaskowski <victor.piaskowski@proton.me>
2025-12-11 16:37:56 +0100

    fluorescent and other calibraotin page change text

--- RosettaX/pages/pages/flourescent_calibration_layout.py
+++ RosettaX/pages/pages/flourescent_calibration_layout.py
@@ -4,7 +4,7 @@ from dash import html, dcc, callback, Input, Output
 dash.register_page(__name__, path='/flourescent_calibration', name='Flourescent Calibration')
 
 layout = html.Div([
-    html.H1('This is our Analytics page'),
+    html.H1('Create and Save A New Flourescent Calibration'),
     html.Div([
         "Select a city: ",
         dcc.RadioItems(
@@ -15,8 +15,13 @@ layout = html.Div([
     ]),
     html.Br(),
     html.Div(id='analytics-output'),
+    html.Br(),
+    html.Label('File Name:'),
+    dcc.Input(id='file-name', type='text', value='Calibration_1.csv'),
+    html.Br(),
+    html.Button('Run Flourescent Calibration', id='run-calibration-button', n_clicks=0),
+    html.Div(id='calibration-result-output'),
 ])
-
 
 @callback(
     Output('analytics-output', 'children'),
@@ -24,3 +29,14 @@ layout = html.Div([
 )
 def update_city_selected(input_value):
     return f'You selected: {input_value}'
+
+@callback(
+    Output('calibration-result-output', 'children'),
+    [Input('run-calibration-button', 'n_clicks'), 
+    Input('file-name', 'value')]  # Assuming there's an input for file name
+)
+def run_flourescent_calibration(n_clicks, file_name):
+    if n_clicks > 0:
+        # Placeholder for actual calibration logic
+        return f'Flourescent Calibration run {n_clicks} times with file name {file_name}.'
+    return ''
--- RosettaX/pages/pages/other_calibration_layout.py
+++ RosettaX/pages/pages/other_calibration_layout.py
@@ -1,26 +1,43 @@
 import dash
 from dash import html, dcc, callback, Input, Output
 
-dash.register_page(__name__, path='/other-calibration', name='Other Calibration')
+dash.register_page(__name__, path='/other_calibration', name='Other Calibration')
 
 layout = html.Div([
-    html.H1('This is our other calibration page'),
+    html.H1('Create and Save A New Other Calibration'),
     html.Div([
         "Select a city: ",
         dcc.RadioItems(
             options=['New York City', 'Montreal', 'San Francisco'],
             value='Montreal',
-            id='analytics-input'
+            id='analytics-input-other_calibration'
         )
     ]),
     html.Br(),
-    html.Div(id='analytics-output2'),
+    html.Div(id='analytics-output-other_calibration'),
+    html.Br(),
+    html.Label('File Name:'),
+    dcc.Input(id='file-name-other_calibration', type='text', value='Calibration_1.csv'),
+    html.Br(),
+    html.Button('Run Other Calibration', id='run-calibration-button-other_calibration', n_clicks=0),
+    html.Div(id='calibration-result-output-other_calibration'),
 ])
 
 
 @callback(
-    Output('analytics-output2', 'children'),
-    Input('analytics-input', 'value')
+    Output('analytics-output-other_calibration', 'children'),
+    Input('analytics-input-other_calibration', 'value')
 )
 def update_city_selected(input_value):
     return f'You selected: {input_value}'
+
+@callback(
+    Output('calibration-result-output-other_calibration', 'children'),
+    [Input('run-calibration-button-other_calibration', 'n_clicks'), 
+    Input('file-name-other_calibration', 'value')]  # Assuming there's an input for file name
+)
+def run_other_calibration(n_clicks, file_name):
+    if n_clicks > 0:
+        # Placeholder for actual calibration logic
+        return f'Other Calibration run {n_clicks} times with file name {file_name}.'
+    return ''
