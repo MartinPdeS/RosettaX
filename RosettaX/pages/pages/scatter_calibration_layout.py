7f4a1f966a747bf0b937fe1450e3c1cbebde02e9
Victor Piaskowski <victor.piaskowski@proton.me>
2025-12-15 15:17:42 -0500

    fluorescent to scatter calibration

--- RosettaX/pages/pages/scatter_calibration_layout.py
+++ RosettaX/pages/pages/scatter_calibration_layout.py
@@ -2,14 +2,14 @@ import dash
 from dash import html, dcc, callback, Input, Output
 import dash_bootstrap_components as dbc
 
-dash.register_page(__name__, path='/flourescent_calibration', name='Flourescent Calibration')
+dash.register_page(__name__, path='/scatter_calibration', name='Scatter Calibration')
 
 layout = html.Div([
-    html.H1('Create and Save A New Flourescent Calibration'),
+    html.H1('Create and Save A New Scatter Calibration'),
     dcc.Upload( id='upload-data',
         children=html.Div([
             'Drag and Drop or ',
-            html.A('Select Files')
+            html.A('Select File')
         ]),
         style={
             'width': '100%',
@@ -22,7 +22,7 @@ layout = html.Div([
             'margin': '10px'
         },
         # Allow multiple files to be uploaded
-        multiple=True
+        multiple=False,
     ),
     html.Br(),
     dbc.Collapse(
@@ -85,40 +85,20 @@ layout = html.Div([
         dcc.Input(id='particle-shell-thickness-input', type='number', value='', placeholder='Particle Shell Thickness (nm)'),
     ]),
     
-    
-    
-    html.Div([
-        "Select a city: ",
-        dcc.RadioItems(
-            options=['New York City', 'Montreal', 'San Francisco'],
-            value='Montreal',
-            id='analytics-input'
-        )
-    ]),
-    html.Br(),
-    html.Div(id='analytics-output'),
-    html.Br(),
     html.Label('File Name:'),
     dcc.Input(id='file-name', type='text', value='Calibration_1.csv'),
     html.Br(),
-    html.Button('Run Flourescent Calibration', id='run-calibration-button', n_clicks=0),
+    html.Button('Run Scatter Calibration', id='run-calibration-button', n_clicks=0),
     html.Div(id='calibration-result-output'),
 ])
-
-@callback(
-    Output('analytics-output', 'children'),
-    Input('analytics-input', 'value')
-)
-def update_city_selected(input_value):
-    return f'You selected: {input_value}'
 
 @callback(
     Output('calibration-result-output', 'children'),
     [Input('run-calibration-button', 'n_clicks'), 
     Input('file-name', 'value')]  # Assuming there's an input for file name
 )
-def run_flourescent_calibration(n_clicks, file_name):
+def run_scatter_calibration(n_clicks, file_name):
     if n_clicks > 0:
         # Placeholder for actual calibration logic
-        return f'Flourescent Calibration run {n_clicks} times with file name {file_name}.'
+        return f'Scatter Calibration run {n_clicks} times with file name {file_name}.'
     return ''
