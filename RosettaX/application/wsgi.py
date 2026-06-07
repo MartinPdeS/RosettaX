053771ba63c765b0d559c66680ed48074a60c01b
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-06-07 18:33:14 +0200

    fixed the scatterer preset removing the measured peaks

--- RosettaX/application/wsgi.py
+++ RosettaX/application/wsgi.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from RosettaX.application.main import RosettaXApplication, configure_logging
+from RosettaX.utils.upload_limits import configure_max_upload_bytes
 
 
 configure_logging(debug=False, log_level="INFO")
+configure_max_upload_bytes(os.getenv("ROSETTAX_MAX_UPLOAD_SIZE"))
 
 application = RosettaXApplication(
     host="0.0.0.0",
