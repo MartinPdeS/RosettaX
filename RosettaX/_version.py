d388ae6695787f3deb91c7eeafe3c0fba699eed1
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-09-08 14:38:15 +0200

    Release v0.6.2

--- RosettaX/_version.py
+++ RosettaX/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.6.1'
-__version_tuple__ = version_tuple = (0, 6, 1)
+__version__ = version = '0.6.2'
+__version_tuple__ = version_tuple = (0, 6, 2)
 
-__commit_id__ = commit_id = 'g991bfa680'
+__commit_id__ = commit_id = None
